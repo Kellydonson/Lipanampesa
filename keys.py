88cfcffa4bcce84d2a594506d1a8487c6011edab
Kellydonson <kwemboikelvin98@gmail.com>
2019-11-07 15:04:38 +0300

    added test_msisdn

---
+++ keys.py
@@ -0,0 +1,7 @@
+business_shortCode = "174379" #Lipa Na Mpesa Shortcode
+phone_number = "254707615758"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "QeUIyUmGZlVDANURAvTCxUZ3AVFzrnHU"
+consumer_secret = "OOAF7ASyoCqsgWTU"
+shortcode = "600268"
+test_msisdn = "254708374149"
